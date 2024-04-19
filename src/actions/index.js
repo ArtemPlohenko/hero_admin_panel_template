40dd73189faea436ba99db12ec579ff7f497868e
artemplohenko@gmail.com <artemplohenko@gmail.com>
2024-04-19 12:34:04 +0300

    added fetchFilters in actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
-import { request } from "http";
+// import { request } from "http";
 
 export const fetchHeroes = (request) => (dispatch) => {
   dispatch(heroesFetching());
   request("http://localhost:3001/heroes")
     .then((data) => dispatch(heroesFetched(data)))
     .catch(() => dispatch(heroesFetchingError()));
+};
+
+export const fetchFilters = (request) => (dispatch) => {
+  dispatch(filtersFetching());
+  request("http://localhost:3001/filters")
+    .then((data) => dispatch(filtersFetched(data)))
+    .catch(() => dispatch(filtersFetchingError()));
 };
 
 export const heroesFetching = () => {
